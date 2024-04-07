d7145790175f4f99b6ac2a5ee6a9350abfe24e92
britney321 <britney.yuen@gmail.com>
2024-04-07 10:43:09 -0700

    fixed shop styling

--- Application/Shop.js
+++ Application/Shop.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { StatusBar, Modal, StyleSheet, Text, View, ScrollView, Image, TouchableOpacity, ImageBackground, Linking, Alert } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 // Custom Header component
 const Title = () => {
@@ -34,10 +35,10 @@ const Category = ({ title }) => {
   );
 };
 
-const Item = ({ imageSource, itemName, onPress, avatarImage, link }) => {
+const Item = ({ imageSource, itemName, onPress, avatarImage, link, description  }) => {
   return (
     <View style={styles.itemContainer}>
-      <TouchableOpacity style={styles.button} onPress={() => onPress(itemName, avatarImage, link)}>
+      <TouchableOpacity style={styles.button} onPress={() => onPress(itemName, avatarImage, link, description)}>
         <ImageBackground source={imageSource} style={styles.buttonBackground}>
           <Text style={styles.buttonText}></Text>
         </ImageBackground>
@@ -51,11 +52,15 @@ const ButtonRow1 = ({ onPress }) => {
   return (
     <View style={styles.buttonRow}>
       <Item imageSource={require('./assets/COELLE SHELL JACKET.png')} itemName="COELLE SHELL JACKET" onPress={onPress} avatarImage={require('./assets/1.png')} 
-      link="https://arcteryx.com/ca/en/shop/womens/coelle-shell-jacket?CMPID=ps%7Cshp%7Cstd%7Cgoogle%7CArc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN%7C%7C%7C151538539338-663122094952&utm_souce=&utm_medium=ps%7Cshp%7Cstd&utm_campaign=Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN&gclsrc=aw.ds&gad_source=1&gclid=Cj0KCQjwiMmwBhDmARIsABeQ7xRGQB92NW5LIyziiyJtrdsoujhdKZWRN-vrqpADgldJiSukxYKsVisaAiIbEALw_wcB"/>
+      link="https://arcteryx.com/ca/en/shop/womens/coelle-shell-jacket?CMPID=ps%7Cshp%7Cstd%7Cgoogle%7CArc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN%7C%7C%7C151538539338-663122094952&utm_souce=&utm_medium=ps%7Cshp%7Cstd&utm_campaign=Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN&gclsrc=aw.ds&gad_source=1&gclid=Cj0KCQjwiMmwBhDmARIsABeQ7xRGQB92NW5LIyziiyJtrdsoujhdKZWRN-vrqpADgldJiSukxYKsVisaAiIbEALw_wcB"
+      description="Ultralight GORE-TEX shell for spring and summer weather protection."/>
       <Item imageSource={require('./assets/LANA MERINO WOOL CREW NECK SHIRT LS.png')} itemName="LANA CREW NECK SHIRT" onPress={onPress} avatarImage={require('./assets/2.png')}
-      link="https://arcteryx.com/ca/en/shop/womens/lana-merino-wool-crew-neck-shirt-ls?CMPID=ps|shp|std|google|Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN|||151538539338-663122094952&utm_souce=&utm_medium=ps|shp|std&utm_campaign=Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN&gclsrc=aw.ds&gad_source=1&gclid=Cj0KCQjwiMmwBhDmARIsABeQ7xQTVjP-Gh4uOCTO3LnJX7emuO1aO7MmaLd_ccgMXNl3F2hskZ4HnusaAulWEALw_wcB"/>
+      link="https://arcteryx.com/ca/en/shop/womens/lana-merino-wool-crew-neck-shirt-ls?CMPID=ps|shp|std|google|Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN|||151538539338-663122094952&utm_souce=&utm_medium=ps|shp|std&utm_campaign=Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN&gclsrc=aw.ds&gad_source=1&gclid=Cj0KCQjwiMmwBhDmARIsABeQ7xQTVjP-Gh4uOCTO3LnJX7emuO1aO7MmaLd_ccgMXNl3F2hskZ4HnusaAulWEALw_wcB"
+      description="Merino-blend crew for performance and comfort across a wide range of activities."/>
+      
       <Item imageSource={require('./assets/SILENE CREW SHIRT.png')} itemName="SILENE CREW SHIRT" onPress={onPress} avatarImage={require('./assets/3.png')}
-      link="https://arcteryx.com/ca/en/shop/womens/silene-crew-shirt-ss?cmpid=pm|mult|perf|google|Arc%27teryx_Google-PMAX_Non-Brand_S23_Performance_BOF_R:NAM_C:CA_L:EN|All_Products||{ad_id}&utm_source=google&utm_medium=pm|mult|perf&utm_campaign=Arc%27teryx_Google-PMAX_Non-Brand_S23_Performance_BOF_R:NAM_C:CA_L:EN&gclsrc=aw.ds&gad_source=1&gclid=Cj0KCQjwiMmwBhDmARIsABeQ7xQpAOxESsRAAboZGwl9Vztd9RAVj_-7RJaSdLVY47vnTvqYMmmhbrwaAoeHEALw_wcB"/>
+      link="https://arcteryx.com/ca/en/shop/womens/silene-crew-shirt-ss?cmpid=pm|mult|perf|google|Arc%27teryx_Google-PMAX_Non-Brand_S23_Performance_BOF_R:NAM_C:CA_L:EN|All_Products||{ad_id}&utm_source=google&utm_medium=pm|mult|perf&utm_campaign=Arc%27teryx_Google-PMAX_Non-Brand_S23_Performance_BOF_R:NAM_C:CA_L:EN&gclsrc=aw.ds&gad_source=1&gclid=Cj0KCQjwiMmwBhDmARIsABeQ7xQpAOxESsRAAboZGwl9Vztd9RAVj_-7RJaSdLVY47vnTvqYMmmhbrwaAoeHEALw_wcB"
+      description="Performance Phasic FL top designed for unrestricted comfort while climbing."/>
     </View>
   );
 };
@@ -64,11 +69,14 @@ const ButtonRow2 = ({ onPress }) => {
   return ( 
     <View style={styles.buttonRow}>
       <Item imageSource={require('./assets/ESSENT HIGH-RISE UTILITY LEGGING.png')} itemName="ESSENT UTILITY LEGGING" onPress={onPress} avatarImage={require('./assets/4.png')}
-      link="https://arcteryx.com/ca/en/shop/womens/essent-high-rise-utility-legging-26?CMPID=ps|shp|std|google|Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN|||151538539338-663122094952&utm_souce=&utm_medium=ps|shp|std&utm_campaign=Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN&gclsrc=aw.ds&gad_source=1&gclid=Cj0KCQjwiMmwBhDmARIsABeQ7xRjkqlmnOY0Cdn7iYp_JWkdh4iS81PnyIk4Cf1JugArq4eyWZcyEgIaAjryEALw_wcB"/>
+      link="https://arcteryx.com/ca/en/shop/womens/essent-high-rise-utility-legging-26?CMPID=ps|shp|std|google|Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN|||151538539338-663122094952&utm_souce=&utm_medium=ps|shp|std&utm_campaign=Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN&gclsrc=aw.ds&gad_source=1&gclid=Cj0KCQjwiMmwBhDmARIsABeQ7xRjkqlmnOY0Cdn7iYp_JWkdh4iS81PnyIk4Cf1JugArq4eyWZcyEgIaAjryEALw_wcB"
+      desription="Stretch, breathability, and pockets in a versatile tight for high-output activities."/>
       <Item imageSource={require('./assets/GAMMA HYBRID PANT.png')} itemName="GAMMA HYBRID PANT" onPress={onPress} avatarImage={require('./assets/5.png')}
-      link="https://arcteryx.com/ca/en/shop/womens/gamma-hybrid-pant?CMPID=ps|shp|std|google|Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN|||151538539338-663122094952&utm_souce=&utm_medium=ps|shp|std&utm_campaign=Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN&gclsrc=aw.ds&gad_source=1&gclid=Cj0KCQjwiMmwBhDmARIsABeQ7xTk48Juet1njOJXKxMm1vLBzY1PW7FRXWIl8_To4VvKqRQVdmua3REaAlO9EALw_wcB"/>
+      link="https://arcteryx.com/ca/en/shop/womens/gamma-hybrid-pant?CMPID=ps|shp|std|google|Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN|||151538539338-663122094952&utm_souce=&utm_medium=ps|shp|std&utm_campaign=Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN&gclsrc=aw.ds&gad_source=1&gclid=Cj0KCQjwiMmwBhDmARIsABeQ7xTk48Juet1njOJXKxMm1vLBzY1PW7FRXWIl8_To4VvKqRQVdmua3REaAlO9EALw_wcB"
+      description="Enhanced mobility and secure storage in a light, durable, breathable hiking pant."/>
       <Item imageSource={require('./assets/NORVAN WINDSHELL PANT.png')} itemName="NORVAN WINDSHELL PANT" onPress={onPress} avatarImage={require('./assets/6.png')}
-      link="https://arcteryx.com/ca/en/shop/womens/norvan-windshell-pant?CMPID=ps|shp|std|google|Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN|||151538539338-663122094952&utm_souce=&utm_medium=ps|shp|std&utm_campaign=Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN&gclsrc=aw.ds&gad_source=1&gclid=Cj0KCQjwiMmwBhDmARIsABeQ7xQgo9GMAUHKwaco93VpNhsbA01eZnGQP7S6qgZ9p5LGOCjn736r5TEaAiaeEALw_wcB"/>
+      link="https://arcteryx.com/ca/en/shop/womens/norvan-windshell-pant?CMPID=ps|shp|std|google|Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN|||151538539338-663122094952&utm_souce=&utm_medium=ps|shp|std&utm_campaign=Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN&gclsrc=aw.ds&gad_source=1&gclid=Cj0KCQjwiMmwBhDmARIsABeQ7xQgo9GMAUHKwaco93VpNhsbA01eZnGQP7S6qgZ9p5LGOCjn736r5TEaAiaeEALw_wcB"
+      description="Ultra-minimalist windshell pant for protection on extended trail runs."/>
     </View>
   );
 };
@@ -77,16 +85,19 @@ const ButtonRow3 = ({ onPress }) => {
   return ( 
     <View style={styles.buttonRow}>
       <Item imageSource={require('./assets/MANTIS BACKPACK.png')} itemName="MANTIS BACKPACK" onPress={onPress} avatarImage={require('./assets/7.png')}
-      link="https://arcteryx.com/ca/en/shop/mantis-26-backpack?CMPID=ps|shp|std|google|Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN|||151538539338-663122094952&utm_souce=&utm_medium=ps|shp|std&utm_campaign=Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN&gclsrc=aw.ds&gad_source=1&gclid=Cj0KCQjwiMmwBhDmARIsABeQ7xSlk174_zOA6dEhmSHpZdEQhrNE17WUeriVZjLWssfDd506iz0Vs78aAvq1EALw_wcB"/>
+      link="https://arcteryx.com/ca/en/shop/mantis-26-backpack?CMPID=ps|shp|std|google|Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN|||151538539338-663122094952&utm_souce=&utm_medium=ps|shp|std&utm_campaign=Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN&gclsrc=aw.ds&gad_source=1&gclid=Cj0KCQjwiMmwBhDmARIsABeQ7xSlk174_zOA6dEhmSHpZdEQhrNE17WUeriVZjLWssfDd506iz0Vs78aAvq1EALw_wcB"
+      description="Comfortable, streamlined, highly versatile 30L pack made with recycled materials."/>
       <Item imageSource={require('./assets/NORVAN LD 3 GTX SHOE.png')} itemName="NORVAN LD 3 GTX SHOE" onPress={onPress} avatarImage={require('./assets/8.png')}
-      link="https://arcteryx.com/ca/en/shop/womens/norvan-ld-3-gtx-shoe?CMPID=ps|shp|std|google|Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN|||151538539338-663122094952&utm_souce=&utm_medium=ps|shp|std&utm_campaign=Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN&gclsrc=aw.ds&gad_source=1&gclid=Cj0KCQjwiMmwBhDmARIsABeQ7xR1QXh94y-fMVMqbPFmDZuTxZm9aQjMvGcDHMw-dx6s_ShaE5xBmO8aApr-EALw_wcB"/>
+      link="https://arcteryx.com/ca/en/shop/womens/norvan-ld-3-gtx-shoe?CMPID=ps|shp|std|google|Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN|||151538539338-663122094952&utm_souce=&utm_medium=ps|shp|std&utm_campaign=Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN&gclsrc=aw.ds&gad_source=1&gclid=Cj0KCQjwiMmwBhDmARIsABeQ7xR1QXh94y-fMVMqbPFmDZuTxZm9aQjMvGcDHMw-dx6s_ShaE5xBmO8aApr-EALw_wcB"
+      description="Lightweight, supportive GORE-TEX shoe for lasting comfort on extended trail runs."/>
       <Item imageSource={require('./assets/BIRD WORD TRUCKER CURVED HAT.png')} itemName="BIRD WORD TRUCKER HAT" onPress={onPress} avatarImage={require('./assets/9.png')}
-      link="https://arcteryx.com/ca/en/shop/bird-word-trucker-curved-hat?CMPID=ps|shp|std|google|Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN|||151538539338-663122094952&utm_souce=&utm_medium=ps|shp|std&utm_campaign=Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN&gclsrc=aw.ds&gad_source=1&gclid=Cj0KCQjwiMmwBhDmARIsABeQ7xR7saAmFegGS9Jt84IdBWoHPRRmZwKj-bYXOh32LyQEihYrIS0AlJoaAit3EALw_wcB"/>
+      link="https://arcteryx.com/ca/en/shop/bird-word-trucker-curved-hat?CMPID=ps|shp|std|google|Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN|||151538539338-663122094952&utm_souce=&utm_medium=ps|shp|std&utm_campaign=Arc%27teryx_Google-Standard-Shopping_S23_Performance_BOF_R:NAM_C:CA_L:EN&gclsrc=aw.ds&gad_source=1&gclid=Cj0KCQjwiMmwBhDmARIsABeQ7xR7saAmFegGS9Jt84IdBWoHPRRmZwKj-bYXOh32LyQEihYrIS0AlJoaAit3EALw_wcB"
+      description="Curved-brim trucker made with light, highly breathable performance materials."/>
       </View>
   );
 };
 
-const OverlayPage = ({ visible, onClose, overlayText, avatarImage, link }) => {
+const OverlayPage = ({ visible, onClose, overlayText, avatarImage, link, description }) => {
   const handleReal = () => {
     Linking.openURL(link);
   };
@@ -99,6 +110,10 @@ const OverlayPage = ({ visible, onClose, overlayText, avatarImage, link }) => {
   };
 
   return (
+    <LinearGradient
+    colors={['#c9a5c3', '#37ab9f', '#40436e']}
+    style={styles.gradientBackground}
+  >
     <Modal
       animationType="slide"
       transparent={false}
@@ -114,14 +129,16 @@ const OverlayPage = ({ visible, onClose, overlayText, avatarImage, link }) => {
       <Text style={styles.overlayText}>{overlayText}</Text>
         <Image source={avatarImage} style={styles.overlayImage} />
         <Text style={styles.priceName}>$20</Text>
-        <TouchableOpacity onPress={handlePurchase}>
+        <Text style={styles.priceName}>{description}</Text>
+        <TouchableOpacity style={styles.purchaseButton}  onPress={handlePurchase}>
           <Text style={styles.purchaseButton} > PURCHASE </Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={handleReal}>
+        <TouchableOpacity style={styles.purchaseButton} onPress={handleReal}>
           <Text style={styles.purchaseButton} > CHECK OUT THE REAL ITEM </Text>
         </TouchableOpacity>
       </View>
     </Modal>
+    </LinearGradient>
   );
 };
 
@@ -130,12 +147,14 @@ export default function Shop() {
   const [overlayText, setOverlayText] = useState('');
   const [overlayImage, setOverlayImage] = useState('');
   const [overlayLink, setOverlayLink] = useState('');
+  const [overlayDescription, setOverlayDescritpion] = useState('');
 
-  const openOverlay = (text, image, link) => {
+  const openOverlay = (text, image, link, description) => {
     setOverlayVisible(true);
     setOverlayText(text);
     setOverlayImage(image);
     setOverlayLink(link);
+    setOverlayDescritpion(description);
   };
 
   const closeOverlay = () => {
@@ -143,6 +162,10 @@ export default function Shop() {
   };
 
   return (
+    <LinearGradient
+      colors={['#c9a5c3', '#37ab9f', '#40436e']}
+      style={styles.gradientBackground}
+    >
     <View style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollViewContent}>
         <Avatar />
@@ -166,15 +189,16 @@ export default function Shop() {
         </View>
       </ScrollView>
       <StatusBar style="auto" />
-      <OverlayPage visible={overlayVisible} onClose={closeOverlay} overlayText={overlayText} avatarImage={overlayImage} link={overlayLink}/>
+      <OverlayPage visible={overlayVisible} onClose={closeOverlay} overlayText={overlayText} avatarImage={overlayImage} link={overlayLink} description={overlayDescription}/>
     </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor: '#fff',
     alignContent: 'center',
   },
   header: {
@@ -191,18 +215,19 @@ const styles = StyleSheet.create({
   },
   headerTitle: {
     fontSize: 30,
-    fontWeight: 'bold',
+    // fontWeight: 'bold',
     color: 'white',
   },
   categoryWrapper: {
-    backgroundColor: '#fff',
+    // backgroundColor: '#fff',
+    opacity: '50%',
     paddingHorizontal: 40,
     paddingVertical: 5,
     borderRadius: 50,
     overflow: 'hidden',
   },
   category: {
-    backgroundColor: 'black',
+    backgroundColor: '#273b47',
     paddingVertical: 20,
     alignItems: 'center',
     justifyContent: 'center',
@@ -237,11 +262,13 @@ const styles = StyleSheet.create({
   },
   purchaseButton: {
     justifyContent: 'center',
-    backgroundColor: 'grey',
+    backgroundColor: 'black',
     color: 'white',
-    paddingVertical: 15, 
+    paddingVertical: 10, 
     paddingHorizontal: 10,
     justifyContent: 'space-around', 
+    borderRadius: 15,
+    fontWeight: 'bold',
   },
   pucahaseButtonBackground: {
     flex: 1,
@@ -262,7 +289,7 @@ const styles = StyleSheet.create({
   },
   priceName: {
     color: 'grey',
-    fontSize: 15,
+    fontSize: 20,
     alignItems: 'center',
   },
   itemContainer: {
@@ -275,7 +302,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0)',
+    // backgroundColor: 'rgba(0, 0, 0, 0)',
     justifyContent: 'space-evenly',
     
   },
@@ -285,7 +312,7 @@ const styles = StyleSheet.create({
   },
   overlayExit: {
     flex: 0.1,
-    backgroundColor: 'rgba(0, 0, 0, 0.0)',
+    // backgroundColor: 'rgba(0, 0, 0, 0.0)',
     paddingLeft: 20, 
     paddingTop: 30,
     height: 10,
