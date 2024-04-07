9c4366004afa7c1cb3715824e6d6306f32d8421a
Alice <cyang41@students.cs.ubc.ca>
2024-04-06 18:47:40 -0700

    community page

--- Application/App.js
+++ Application/App.js
@@ -1,14 +1,247 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Image } from 'react-native';
+import { Button, Card, Header, Tab, TabView, Icon } from '@rneui/themed';
+
+const users = [
+  {
+    name: 'brynn',
+    avatar: 'https://uifaces.co/our-content/donated/1H_7AxP0.jpg',
+    title: 'title',
+    img: '',
+    captions: 'captions',
+  },
+  {
+    name: 'thot leader',
+    avatar:
+      'https://images.pexels.com/photos/598745/pexels-photo-598745.jpeg?crop=faces&fit=crop&h=200&w=200&auto=compress&cs=tinysrgb',
+    title: 'title',
+    img: '',
+    captions: 'captions',
+  },
+  {
+    name: 'jsa',
+    avatar: 'https://uifaces.co/our-content/donated/bUkmHPKs.jpg',
+    title: 'title',
+    img: '',
+    captions: 'captions',
+  },
+  {
+    name: 'talhaconcepts',
+    avatar: 'https://randomuser.me/api/portraits/men/4.jpg',
+    title: 'title',
+    img: '',
+    captions: 'captions',
+  },
+  {
+    name: 'andy vitale',
+    avatar: 'https://uifaces.co/our-content/donated/NY9hnAbp.jpg',
+    title: 'title',
+    img: '',
+    captions: 'captions',
+  },
+  {
+    name: 'katy friedson',
+    avatar:
+      'https://images-na.ssl-images-amazon.com/images/M/MV5BMTgxMTc1MTYzM15BMl5BanBnXkFtZTgwNzI5NjMwOTE@._V1_UY256_CR16,0,172,256_AL_.jpg',
+    title: 'title',
+    img: '',
+    captions: 'captions',
+  },
+  ];
 
 export default function App() {
+  const [visible, setVisible] = React.useState(true);
+  const [index, setIndex] = React.useState(0);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    
+
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    //   <Button
+    //           title="To community"
+    //           buttonStyle={{
+    //             backgroundColor: 'rgba(78, 116, 289, 1)',
+    //             borderRadius: 3,
+    //           }}
+    //           containerStyle={{
+    //             width: 200,
+    //             marginHorizontal: 50,
+    //             marginVertical: 10,
+    //           }}
+    //         />
+    // </View>
+
+    <View >
+      <Header
+      //style={styles.container}
+      backgroundImageStyle={{}}
+      barStyle="default"
+      centerComponent={{
+        text: "Hiking Buddy",
+        style: { color: "#fff", 
+        fontSize: 20,
+        fontWeight: 'bold',
+        //textAlign:'center',
+        textAlignVertical: 'bottom'},
+      }}
+      centerContainerStyle={{}}
+      containerStyle={{ height:100}}
+      leftContainerStyle={{}}
+      linearGradientProps={{}}
+      placement="center"
+      rightContainerStyle={{}}
+      statusBarProps={{}}
+    />
+    
+      <Button
+              title="POST"
+              icon={{
+                name: 'plus',
+                type: 'font-awesome',
+                size: 15,
+                color: 'white',
+              }}
+              iconRight
+              iconContainerStyle={{ marginLeft: 20 }}
+              titleStyle={{ fontWeight: 'bold' }}
+              buttonStyle={{
+                backgroundColor: 'rgba(199, 43, 98, 1)',
+                borderColor: 'transparent',
+                width: "70%",
+                borderWidth: 0,
+                borderRadius: 30,
+                placement: 'right'
+              }}
+              containerStyle={{
+                width: "auto",
+                //placement: 'right',
+                marginVertical: 20,
+                marginEnd: -30,
+                alignSelf: 'flex-end'
+              }}
+            />
+
+
+      <ScrollView>
+        {/* <View styles={styles.container} >
+        <Card>
+          <Card.Title>CARD WITH DIVIDER</Card.Title>
+          <Card.Divider />
+          {users.map((u, i) => {
+            return (
+              <View key={i} style={styles.user}>
+                <Image
+                  style={styles.image}
+                  resizeMode="cover"
+                  source={{ u: u.avatar }}
+                />
+                <Text style={styles.name}>{u.name}</Text>
+              </View>
+            );
+          })}
+        </Card>
+        </View> */}
+
+        <View styles={styles.container} >
+        {users.map((u, i) => {
+            return (
+              
+              <Card key={i}>
+          <Card.Title>{u.title}</Card.Title>
+          <Card.Divider />
+          <Card.Image
+            style={{ padding: 0 }}
+            source={{
+              uri:
+                'https://awildgeographer.files.wordpress.com/2015/02/john_muir_glacier.jpg',
+            }}
+          />
+          <Text style={{ marginBottom: 10 }}>
+            {u.captions}
+          </Text>
+          <Button
+            icon={
+              <Icon
+                name="code"
+                color="#ffffff"
+                iconStyle={{ marginRight: 10 }}
+              />
+            }
+            buttonStyle={{
+              borderRadius: 0,
+              marginLeft: 0,
+              marginRight: 0,
+              marginBottom: 0,
+            }}
+            title="Details"
+          />
+        </Card>
+            );
+          })}
+          </View>
+
+          
+
+      </ScrollView>  
+
+      <Button>
+          </Button>
+
+
+
+
+
+
+      {/* <Tab
+      value={index}
+      onChange={(e) => setIndex(e)}
+      indicatorStyle={{
+        backgroundColor: 'white',
+        height: 3,
+      }}
+      variant="primary"
+    >
+      <Tab.Item
+        title="Recent"
+        titleStyle={{ fontSize: 12 }}
+        icon={{ name: 'timer', type: 'ionicon', color: 'white' }}
+      />
+      <Tab.Item
+        title="favorite"
+        titleStyle={{ fontSize: 12 }}
+        icon={{ name: 'heart', type: 'ionicon', color: 'white' }}
+      />
+      <Tab.Item
+        title="cart"
+        titleStyle={{ fontSize: 12 }}
+        icon={{ name: 'cart', type: 'ionicon', color: 'white' }}
+      />
+    </Tab>
+
+    <TabView value={index} onChange={setIndex} animationType="spring">
+      <TabView.Item style={{ backgroundColor: 'red', width: '100%' }}>
+        <Text h1>Recent</Text>
+      </TabView.Item>
+      <TabView.Item style={{ backgroundColor: 'blue', width: '100%' }}>
+        <Text h1>Favorite</Text>
+      </TabView.Item>
+      <TabView.Item style={{ backgroundColor: 'green', width: '100%' }}>
+        <Text h1>Cart</Text>
+      </TabView.Item>
+    </TabView> */}
+
+
+
     </View>
+    
+
+    
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +249,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  fonts: {
+    marginBottom: 8,
+  },
+  user: {
+    flexDirection: 'row',
+    marginBottom: 6,
+  },
+  image: {
+    width: 30,
+    height: 30,
+    marginRight: 10,
+  },
+  name: {
+    fontSize: 16,
+    marginTop: 5,
   },
 });
