5f871b448ccb10504d89bd0d9a19164f1003736c
porkcons <77551760+Techeleno@users.noreply.github.com>
2024-04-06 19:28:30 -0700

    Sadness

--- Application/App.js
+++ Application/App.js
@@ -1,20 +1,101 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber/native';
 
-export default function App() {
+function Box(props) {
+  const meshRef = useRef(null);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  useFrame((state, delta) => (meshRef.current.rotation.x += delta));
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Sphere(props) {
+  const meshRef = useRef(null);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  useFrame((state, delta) => (meshRef.current.rotation.y += delta));
+
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+      <sphereGeometry args={[1, 32, 32]} />
+      <meshStandardMaterial color={hovered ? 'skyblue' : 'lightgreen'} />
+    </mesh>
+  );
+}
+
+function Cylinder(props) {
+  const meshRef = useRef(null);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  useFrame((state, delta) => (meshRef.current.rotation.z += delta));
+
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+      <cylinderGeometry args={[0.5, 0.5, 1, 32]} />
+      <meshStandardMaterial color={hovered ? 'lightcoral' : 'lightblue'} />
+    </mesh>
+  );
+}
+
+function Torus(props) {
+  const meshRef = useRef(null);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  useFrame((state, delta) => (meshRef.current.rotation.x += delta));
+
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+      <torusGeometry args={[0.5, 0.1, 16, 100]} />
+      <meshStandardMaterial color={hovered ? 'violet' : 'lightyellow'} />
+    </mesh>
+  );
+}
+
+export default function App() {
+  return (
+    <Canvas
+      style={{ background: '#c4d6a3' }}>
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <Box position={[-3, 0, 0]} />
+      <Sphere position={[0, 0, 0]} />
+      <Cylinder position={[3, 0, 0]} />
+      <Torus position={[0, 3, 0]} />
+    </Canvas>
+  );
+}
